e23a222002502421a8227c8549c57739fac94624
SunnyLLL <92508027+SunnyLLL@users.noreply.github.com>
2022-03-13 05:09:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, Service is listening !');
+  res.send('Hello, Service is listening,Monday !');
 });
 
 app.use('/v1', router);
